4aba53f2ea834c6182281c97ec9fd4597391175d
Dirijable <checkpuk1379@gmail.com>
2026-07-02 00:30:01 +0300

    feat: update register handler method

--- internal/handler/device.go
+++ internal/handler/device.go
@@ -2,23 +2,42 @@ package handler
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"tracker-server/internal/dto"
+	"tracker-server/internal/handler/decode"
+	"tracker-server/internal/handler/encode"
 )
 
 type DeviceRegistrar interface {
-	Register(ctx context.Context, code string) (string, error)
+	Register(ctx context.Context, request dto.RegisterDeviceRequest) (dto.RegisterDeviceResponse, error)
 }
 
 type DeviceHandler struct {
-	r DeviceRegistrar
+	deviceRegistrar DeviceRegistrar
 }
 
 func NewDeviceHandler(r DeviceRegistrar) *DeviceHandler {
 	return &DeviceHandler{
-		r: r,
+		deviceRegistrar: r,
 	}
 }
 
 func (h *DeviceHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var regDto dto.RegisterDeviceRequest
+	if err := decode.Decode(r, &regDto); err != nil {
+		//TODO err response
+		log.Println(err)
+		return
+	}
+	token, err := h.deviceRegistrar.Register(r.Context(), regDto)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
+	if err := encode.SendJSONResponse(w, http.StatusCreated, token); err != nil {
+		log.Println(err)
+		return
+	}
 }
